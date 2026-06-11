47a06371fdb0f12fdc88be5b3c0f627d99894128
Samuel Macêdo <svm.macedo@gmail.com>
2026-06-11 23:33:31 +0000

    [feat]: created download_history.

---
+++ functions/data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import yfinance as yf
+
+def download_history(
+    ticker:str,
+    multi_level_index:bool = False
+)-> pd.DataFrame:
+    """ 
+    Download historical data from yahoo finance.
+
+    Args:
+        ticker (str): Ticker name.
+        multi_level_inde (bool): Remove/include multi index.    
+    """
+
+    df = yf.download(
+        tickers = ticker,
+        multi_level_index = multi_level_index
+    )
+
+    return df    
